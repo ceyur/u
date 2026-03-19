c0e9adf06a42f5ce29b625d23203c8bd00f7a4bc
ceyur <139768476+ceyur@users.noreply.github.com>
2026-03-19 20:40:22 +0500

    Update uiContainer.js

--- uiContainer.js
+++ uiContainer.js
@@ -2,18 +2,18 @@ import { navigate } from "./router.js";
 
 // Инициализация базового UI
 export function initUI() {
-  const app = document.getElementById("app");
-  app.innerHTML = `
-    <header style="padding: 16px; background-color: #1e1e1e; color: #fff; display: flex; justify-content: space-between;">
-      <h1>Smart Dashboard</h1>
-      <nav>
- //       <button data-path="/tasks">Tasks</button>
- //       <button data-path="/notes">Notes</button>
- //       <button data-path="/tracker">Tracker</button>
-      </nav>
-    </header>
-    <main id="main-content" style="padding: 16px;"></main>
-  `;
+ //  const app = document.getElementById("app");
+ //  app.innerHTML = `
+ //    <header style="padding: 16px; background-color: #1e1e1e; color: #fff; display: flex; justify-content: space-between;">
+ //      <h1>Smart Dashboard</h1>
+ //      <nav>
+ //      <button data-path="/tasks">Tasks</button>
+ //        <button data-path="/notes">Notes</button>
+ //        <button data-path="/tracker">Tracker</button>
+ //      </nav>
+ //    </header>
+ //    <main id="main-content" style="padding: 16px;"></main>
+ //  `;
 
   // Привязка кнопок навигации
   const buttons = app.querySelectorAll("button[data-path]");
