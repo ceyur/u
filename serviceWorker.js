fffdff94dc568f5b1c23df23de390099e97c917b
ceyur <139768476+ceyur@users.noreply.github.com>
2026-03-19 20:26:44 +0500

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -3,12 +3,12 @@ const CACHE_NAME = "smart-dashboard-v1";
 const ASSETS_TO_CACHE = [
   "/",
   "/index.html",
-  "/offline.html",
+//  "/offline.html",
   "/manifest.json",
 
   "/main.js",
 
-  / "/src/styles/reset.css",
+  // "/src/styles/reset.css",
   // "/src/styles/variables.css",
   // "/src/styles/main.css"
 ];
