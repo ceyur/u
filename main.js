f2106908f0533ffb158e6e1c988c8fd9556ac19a
ceyur <139768476+ceyur@users.noreply.github.com>
2026-04-11 14:52:46 +0500

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function go(title, name) {
 		p.innerHTML = "<b>" + e.name + "</b>: " + e.value + "; – " + e.description +"<br><i>" + e.name + ": " + e.example + ";" + "</i> " + "(" + e.default + ")";
 		content.append(p);
 	});
-	console.log("/u/" + name);
+	console.log("/u/" + name + "/");
 };
 
 const routes = {
@@ -26,7 +26,7 @@ const routes = {
 		document.title = "Свойства элементов css";
 		h1.textContent = "Свойства элементов css";
 		content.innerHTML = "";
-		console.log("/u");
+		console.log("/u/");
 	},
 	"information": () => {
 		information.style.display = "block";
@@ -34,19 +34,19 @@ const routes = {
 		document.querySelector("#information+a+*").style.position = "fixed";
 		document.querySelector("#information+a+*").style.width = "100%";
 	},
-	"/u/text": () => go("Свойства текста", "text"),
-	"/u/elements": () => go("Свойства элементов", "elements"),
-	"/u/border": () => go("Свойства границы", "border"),
-	"/u/background": () => go("Свойства фона", "background"),
-	"/u/position": () => go("Позиционирование", "position"),
-	"/u/scrollbar": () => go("Свойства скроллбара", "scrollbar"),
-	"/u/flex-containers": () => go("Флекс-контейнеры", "flex_containers"),
-	"/u/flex-elements": () => go("Флекс-элементы", "flex_elements"),
-	"/u/pseudoclasses": () => go("Псевдоклассы", "pseudoclasses"),
-	"/u/pseudoelements": () => go("Псевдоэлементы", "pseudoelements"),
-	"/u/transitions": () => go("Переходы", "transitions"),
-	"/u/animation": () => go("Анимация", "animation"),
-	"/u/transform": () => go("Трансформация", "transform")
+	"/u/text/": () => go("Свойства текста", "text"),
+	"/u/elements/": () => go("Свойства элементов", "elements"),
+	"/u/border/": () => go("Свойства границы", "border"),
+	"/u/background/": () => go("Свойства фона", "background"),
+	"/u/position/": () => go("Позиционирование", "position"),
+	"/u/scrollbar/": () => go("Свойства скроллбара", "scrollbar"),
+	"/u/flex-containers/": () => go("Флекс-контейнеры", "flex_containers"),
+	"/u/flex-elements/": () => go("Флекс-элементы", "flex_elements"),
+	"/u/pseudoclasses/": () => go("Псевдоклассы", "pseudoclasses"),
+	"/u/pseudoelements/": () => go("Псевдоэлементы", "pseudoelements"),
+	"/u/transitions/": () => go("Переходы", "transitions"),
+	"/u/animation/": () => go("Анимация", "animation"),
+	"/u/transform/": () => go("Трансформация", "transform")
 };
 const defaultRoute = "/u/";
 const content = document.querySelector("#content");
@@ -92,12 +92,23 @@ function initRouter() {
 }
 
 // Обработка маршрута
+// function handleRoute() {
+// 	console.log("window.location.pathname: ", window.location.pathname);
+// 	const path = window.location.pathname;
+// 	const route = routes[path] ? path : defaultRoute;
+// 	routes[route]();
+// }
 function handleRoute() {
 	console.log("window.location.pathname: ", window.location.pathname);
-	const path = window.location.pathname;
-	const route = routes[path] ? path : defaultRoute;
+	const path = window.location.pathname.split("/");
+	const n = path.at(-1) == "information" ? -2 : -1;
+	const route = routes[path.at(n)] ? path.at(n) : defaultRoute;
 	routes[route]();
+	if (n == -2) {
+		routes.information();
+	}
 }
+
 
 // Инициализация базового UI
 function initUI() {
@@ -127,97 +138,91 @@ function initUI() {
 	};
 	const button_info = document.querySelector("#button_info");
 	button_info.onclick = () => {
-		information.style.display = "block";
-		document.querySelector("#information+a").style.display = "block";
-		document.querySelector("#information+a+*").style.position = "fixed";
-		document.querySelector("#information+a+*").style.width = "100%";
-		const path = window.location.pathname + "information";
+		const path = window.location.pathname + "information/";
 		history.pushState({}, "", path);
+		handleRoute();
 		console.log("button_info.click");
 	};
 	const close_info = document.querySelector("#close_info");
 	close_info.onclick = () => {
-		information.style.display = "none";
-		document.querySelector("#information+a").style.display = "none";
-		document.querySelector("#information+a+*").style.position = "relative";
-		document.querySelector("#information+a+*").style.width = "auto";
-		const path = window.location.pathname.replace("information", "");
+		const path = window.location.pathname.replace("information/", "");
 		history.pushState({}, "", path);
+		handleRoute();
 		console.log("button_info.click");
 	};
 	const text = document.querySelector("#a1");
 	text.onclick = () => {
-		const path = "/u/text";
+		const path = "/u/text/";
 		history.pushState({}, "", path);
 		handleRoute();
 		console.log("text.click");
 	};
 	const elements = document.querySelector("#elements");
 	elements.onclick = () => {
-		const path = "/u/elements";
+		const path = "/u/elements/";
 		history.pushState({}, "", path);
 		handleRoute();
 		console.log("elements.click");
 	};
 	const border = document.querySelector("#border");
 	border.onclick = () => {
-		const path = "/u/border";
+		const path = "/u/border/";
 		history.pushState({}, "", path);
 		handleRoute();
 		console.log("border.click");
 	};
 	const background = document.querySelector("#a4");
 	background.onclick = () => {
-		const path = "/u/background";
+		const path = "/u/background/";
 		history.pushState({}, "", path);
 		handleRoute();
 		console.log("background.click");
 	};
 	const position = document.querySelector("#a5");
 	position.onclick = () => {
-		const path = "/u/position";
+		const path = "/u/position/";
 		history.pushState({}, "", path);
 		handleRoute();
 		console.log("position.click");
 	};
 	const scrollbar = document.querySelector("#scrollbar");
 	scrollbar.onclick = () => {
-		const path = "/u/scrollbar";
+		const path = "/u/scrollbar/";
 		history.pushState({}, "", path);
 		handleRoute();
 		console.log("scrollbar.click");
 	};
 	const flex_containers = document.querySelector("#a7");
 	flex_containers.onclick = () => {
-		const path = "/u/flex-containers";
+		const path = "/u/flex-containers/";
 		history.pushState({}, "", path);
 		handleRoute();
 		console.log("flex-containers.click");
 	};
 	const flex_elements = document.querySelector("#flex-elements");
 	flex_elements.onclick = () => {
-		const path = "/u/flex-elements";
+		const path = "/u/flex-elements/";
 		history.pushState({}, "", path);
 		handleRoute();
 		console.log("flex-elements.click");
 	};
 	const pseudoclasses = document.querySelector("#a9");
 	pseudoclasses.onclick = () => {
-		const path = "/u/pseudoclasses";
+		const path = "/u/pseudoclasses/";
 		history.pushState({}, "", path);
 		handleRoute();
 		console.log("pseudoclasses.click");
 	};
 	const pseudoelements = document.querySelector("#a10");
 	pseudoelements.onclick = () => {
-		const path = "/u/pseudoelements";
+		const path = "/u/pseudoelements/";
 		history.pushState({}, "", path);
 		handleRoute();
 		console.log("pseudoelements.click");
 	};
 	const transitions = document.querySelector("#transitions");
 	transitions.onclick = () => {
-		const path = "/u/transitions";
+		const path = "/u/transitions/";
 		console.log("transitions.click");
 		// Переход по маршруту
 		history.pushState({}, "", path);
@@ -225,14 +230,14 @@ function initUI() {
 	};
 	const animation = document.querySelector("#animation");
 	animation.onclick = () => {
-		const path = "/u/animation";
+		const path = "/u/animation/";
 		history.pushState({}, "", path);
 		handleRoute();
 		console.log("animation.click");
 	};
 	const transform = document.querySelector("#a13");
 	transform.onclick = () => {
-		const path = "/u/transform";
+		const path = "/u/transform/";
 		history.pushState({}, "", path);
 		handleRoute();
 		console.log("transform.click");
