42d63b58c5555ca700d9fdc408eb1cd8ac1e46be
ceyur <139768476+ceyur@users.noreply.github.com>
2026-04-13 17:59:51 +0500

    Update main.js

--- main.js
+++ main.js
@@ -126,7 +126,7 @@ function go(name) {
 	Object.values(properties[name]).forEach((e) => {
 		if (e == properties[name].title) return;
 		let p = document.createElement("p");
-		p.innerHTML = "<b>${e.name}</b>: ${e.value}; – ${e.description}<br><i>${e.name}: ${e.example};</i> (${e.default})";
+		p.innerHTML = `<b>${e.name}</b>: ${e.value}; – ${e.description}<br><i>${e.name}: ${e.example};</i> (${e.default})`;
 		content.append(p);
 	});
 }
