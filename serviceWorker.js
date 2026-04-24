7f38a24ff658f97938abf08c58ff9e2e52ca6826
ceyur <139768476+ceyur@users.noreply.github.com>
2026-04-24 17:38:08 +0500

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,17 +1,16 @@
 const cacheName = "v1";
 const filesForCache = [
-//  "./",
-//  "./index.html",
-//  "./css/styles.css",
-//  "./scripts.js",
-//  "./manifest.json",
-  "./properties.json"
+ // "./",
+ // "./index.html",
+ // "./css/styles.css",
+ // "./scripts.js",
+ // "./manifest.json",
+ //  "./properties.json"
 ];
 
-// Установка Service Worker
 self.addEventListener("install", event => {
   console.log("Service Worker ", cacheName, " installing...");
-
+  
   event.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll(filesForCache);
@@ -19,7 +18,6 @@ self.addEventListener("install", event => {
   );
 });
 
-// Активация (очистка старых кэшей)
 self.addEventListener("activate", event => {
   console.log("Service Worker ", cacheName, " activating...");
 
@@ -40,14 +38,11 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      // Если есть в кэше — возвращаем
       if (response) {
         return response;
       }
 
-     // Иначе пробуем из сети
       return fetch(event.request).catch(() => {
-        // Если нет интернета — показываем offline страницу
         if (event.request.mode === "navigate") {
           return caches.match("/index.html");
         }
