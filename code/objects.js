cee1cde442234f5d62df1c2549c3cb6237d0c4c3
ceyur <139768476+ceyur@users.noreply.github.com>
2026-07-16 13:05:30 +0500

    Update objects.js

---
+++ code/objects.js
@@ -0,0 +1,13 @@
+export default {
+  parallelogram: {
+    html: "<div id="parallelogram"></div>",
+    css: `#parallelogram {
+      width: 150px;
+    	height: 112px;
+    	background: black;
+    	margin: 0 10px;
+    	transform: skew(-10deg);
+    }`,
+    js: ""
+  }
+}
