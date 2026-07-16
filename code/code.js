9f294169eb48b6e53d398380f7179d67299208f7
ceyur <139768476+ceyur@users.noreply.github.com>
2026-07-16 13:40:06 +0500

    Update code.js

---
+++ code/code.js
@@ -0,0 +1,20 @@
+import objects from "/u/code/objects.js";
+const code = document.querySelector("#code");
+Object.keys(objects).forEach(key => {
+  let iframe = document.createElement("iframe");
+  iframe.id = key;
+  iframe.srcdoc = `${objects[key].html}
+  <style>
+  html,
+  body {
+  	margin: 0;
+  	height: 100%;
+  }
+  body {
+  	display: flex;
+  	justify-content: center;
+  	align-items: center;
+  }
+${objects[key].css}</style>`;
+  code.appendChild(iframe);
+});
