4d4ba990205f3a38789f74c8910f44c3fe133cd7
ceyur <139768476+ceyur@users.noreply.github.com>
2026-07-16 14:49:06 +0500

    Update objects.js

--- code/objects.js
+++ code/objects.js
@@ -9,5 +9,62 @@ export default {
     	transform: skew(-10deg);
     }`,
     "js": ""
+  },
+  "rhomb": {
+    "html": `<div id="rhomb"></div>`,
+    "css": `#rhomb {
+    	width: 100px;
+    	height: 100px;
+    	background: black;
+    	margin: 19.3px;
+    	rotate: 45deg;
+    }`,
+    "js": ""
+  },
+  "trapezoid": {
+    "html": `<div id="trapezoid"></div>`,
+    "css": `#trapezoid {
+    	width: 150px;
+    	border-bottom: 100px solid black;
+    	border-left: 30px solid transparent;
+    	border-right: 30px solid transparent;
+    }`,
+    "js": ""
+  },
+  "triangle-top": {
+    "html": `<div id="triangle-top"></div>`,
+    "css": `#triangle-top {
+    	border-bottom: 84px solid black;
+    	border-right: 50px solid transparent;
+    	border-left: 50px solid transparent;
+    }`,
+    "js": ""
+  },
+  "triangle-right": {
+    "html": `<div id="triangle-right"></div>`,
+    "css": `#triangle-right {
+    	border-left: 80px solid black;
+    	border-top: 42px solid transparent;
+    	border-bottom: 42px solid transparent;
+    }`,
+    "js": ""
+  },
+  "triangle-bottom": {
+    "html": `<div id="triangle-bottom"></div>`,
+    "css": `#triangle-bottom {
+    	border-top: 84px solid black;
+    	border-right: 50px solid transparent;
+    	border-left: 50px solid transparent;
+    }`,
+    "js": ""
+  },
+  "triangle-left": {
+    "html": `<div id="triangle-left"></div>`,
+    "css": `#triangle-left {
+    	border-right: 80px solid black;
+    	border-top: 42px solid transparent;
+    	border-bottom: 42px solid transparent;
+    }`,
+    "js": ""
   }
 }
