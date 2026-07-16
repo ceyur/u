d43deaef01e118c89ef92d662322917936f0424c
ceyur <139768476+ceyur@users.noreply.github.com>
2026-07-16 14:21:13 +0500

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -111,7 +111,7 @@ function goCode() {
 	close.style.display = "flex";
 	document.title = "Готовый код";
 	h1.innerHTML = "Готовый код";
-	import "/u/code/code.js";
+	import("/u/code/code.js");
 	window.scrollTo(0, 0);
 }
 
