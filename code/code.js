98f065b29dcfd6cce35d98e6be4e0833d09b5e27
ceyur <139768476+ceyur@users.noreply.github.com>
2026-07-18 13:26:54 +0500

    Update code.js

--- code/code.js
+++ code/code.js
@@ -1,11 +1,7 @@
 import objects from "/u/code/objects.js";
 const code = document.querySelector("#code");
-Object.keys(objects).forEach(key => {
-  let iframe = document.createElement("iframe");
-  iframe.id = key;
-  iframe.srcdoc = `${objects[key].html}
-  <style>
-  html,
+
+let styleInIframe = `html,
   body {
   	margin: 0;
   	height: 100%;
@@ -14,11 +10,18 @@ Object.keys(objects).forEach(key => {
   	display: flex;
   	justify-content: center;
   	align-items: center;
-  }
-  ${objects[key].css}</style>`;
+  }`;
+
+Object.keys(objects).forEach(key => {
+  let iframe = document.createElement("iframe");
+  iframe.id = key;
+  iframe.srcdoc = `${objects[key].html}
+  <style>${styleInIframe}${objects[key].css}</style>`;
   code.appendChild(iframe);
 
   let div = document.createElement("div");
-  div.textContent = objects[key].html;
+  div.innerHTML = `<p class="html">${objects[key].html}</p>
+  <p class="css" style="display="none">${objects[key].css}</p>
+  <p class="js" style="display="none">${objects[key].js}</p>`;
   code.appendChild(div);
 });
