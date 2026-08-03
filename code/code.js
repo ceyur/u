a066d9e2d370576346c0a8a1bdf5b15e44d81113
ceyur <139768476+ceyur@users.noreply.github.com>
2026-08-03 18:54:02 +0500

    Update code.js

--- code/code.js
+++ code/code.js
@@ -45,12 +45,12 @@ Object.keys(objects).forEach(key => {
       button.querySelector("svg").style.scale = "1 -1";
       div1.style.display = "block";
       div1.querySelector(".css").style.display = "block";
-      div1.querySelector(".css").style.heigth = "auto";
-      div1.querySelector(".css").style.marginBottom = "25px";
+      div1.querySelector(".css").style.height = "auto";
+      div1.querySelector(".css").style.marginTop = "25px";
       if (objects[key].js != "") {
         div1.querySelector(".js").style.display = "block";
         div1.querySelector(".js").style.height = "auto";
-        div1.querySelector(".js").style.marginBottom = "25px";
+        div1.querySelector(".js").style.marginTop = "25px";
       }
     }
   };
