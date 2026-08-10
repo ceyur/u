794108700512d854488a5679a6eb850734d7bc0a
ceyur <139768476+ceyur@users.noreply.github.com>
2026-08-10 17:59:20 +0500

    Update objects.js

--- code/objects.js
+++ code/objects.js
@@ -67,7 +67,7 @@ export default {
 }`,
 		"js": ""
 	},
-	"": {
+	"close-square": {
 		"html": `<a id="close-square"></a>`,
 		"css": `#close-square {
 	display: flex;
@@ -101,5 +101,135 @@ export default {
 	}
 }`,
 		"js": ""
+	},
+	"close-round": {
+		"html": `<a id="close-round"></a>`,
+		"css": `#close-round {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 100px;
+	height: 100px;
+	box-shadow: 0 0 10px #000;
+	border-radius: 50%;
+	transition: 0.25s;
+	&:before, 
+	&:after {
+		content: "";
+		position: absolute;
+		width: 75px;
+		height: 10px;
+		background: #000;
+		border-radius: 5px;
+		rotate: 45deg;
 	}
-}
+	&:after {
+		rotate: -45deg;
+	}
+	&:hover {
+		background: #f9e3e2;
+		box-shadow: 0 0 10px #8b0000;
+		scale: 1.15;
+		&:before, 
+		&:after {
+			background: red;
+		}
+	}
+}`,
+		"js": ""
+	},
+	"arrow-square": {
+		"html": `<a id="arrow-square"></a>`,
+		"css": `#arrow-square {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 100px;
+	height: 100px;
+	box-shadow: 0 0 10px #000;
+	border-radius: 50%;
+	transition: 0.25s;
+	&:before {
+		content: "";
+		position: absolute;
+		width: 30px;
+		height: 30px;
+		border-top: 10px solid #000;
+		border-left: 10px solid #000;
+		rotate: 45deg;
+	}
+	&:after {
+		content: "";
+		position: absolute;
+		width: 10px;
+		height: 51px;
+		background: #000;
+		margin-top: 6px;
+	}
+	&:hover {
+		background: #9cdaff;
+		box-shadow: 0 0 10px #00008b
+		scale: 1.15;
+		&:before {
+			border-color: blue;
+		}
+		&:after {
+			background: blue;
+		}
+	}
+}`,
+		"js": ""
+	},
+	"arrow-round": {
+		"html": `<a id="arrow-round"></a>`,
+		"css": `#arrow-round {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 100px;
+	height: 100px;
+	box-shadow: 0 0 10px #000;
+	border-radius: 50%;
+	transition: 0.25s;
+	u {
+		position: absolute;
+		width: 10px;
+		height: 51px;
+		background: #000;
+		border-radius: 9px;
+		margin-top: 6px;
+	}
+	&:before {
+		content: "";
+		position: absolute;
+		width: 10px;
+		height: 40px;
+		background: #000;
+		border-radius: 9px;
+		margin: 0 21.6px 21.6px 0;
+		rotate: 45deg;
+	}
+	&:after {
+		content: "";
+		position: absolute;
+		width: 10px;
+		height: 40px;
+		background: #000;
+		border-radius: 9px;
+		margin: 0 0 21.6px 21.6px;
+		rotate: -45deg;
+	}
+	&:hover {
+		background: #9cdaff;
+		box-shadow: 0 0 10px #00008b;
+		scale: 1.15;
+		u, 
+		&:before, 
+		&:after {
+			background: blue;
+		}
+	}
+}`,
+		"js": ""
+	}
+};
