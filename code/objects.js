54e0f79e368b4c59c0fcaf1a9df90446f5548f57
ceyur <139768476+ceyur@users.noreply.github.com>
2026-08-12 10:50:04 +0500

    Update objects.js

--- code/objects.js
+++ code/objects.js
@@ -228,5 +228,290 @@ export default {
 	}
 }`,
 		"js": ""
+	},
+	"switch-light": {
+		"html": `<input id="switch-light" type="checkbox">`,
+		"css": `#switch-light {
+	visibility: hidden;
+	position: relative;
+	width: 120px;
+	height: 68px;
+	cursor: pointer;
+	-webkit-tap-highlight-color: transparent;
+	&:before {
+		content: "";
+		display: block;
+		visibility: visible;
+		width: 120px;
+		height: 68px;
+		background: linear-gradient(white, #eee);
+		box-shadow: 0 0 4px black;
+		border-radius: 34px;
+		transition: 0.25s;
+	}
+	&:after {
+		content: "";
+		display: block;
+		visibility: visible;
+		position: absolute;
+		top: 0;
+		width: 52px;
+		height: 52px;
+		margin: 8px;
+		background: linear-gradient(white, #ddd);
+		box-shadow: inset 0 0 2px #999;
+		border-radius: 50%;
+		transition: 0.25s;
+	}
+	&:hover {
+		&:before {
+			box-shadow: 0 0 10px black;
+		}
+	}
+	&:checked {
+		&:before {
+			background: #222;
+		}
+		&:after {
+			left: 52px;
+			background: linear-gradient(#777, #3a3a3a);
+			box-shadow: none;
+		}
+	}
+}`,
+		"js": ""
+	},
+	"switch-day": {
+		"html": `<input id="switch-day" type="checkbox">`,
+		"css": `#switch-day {
+	visibility: hidden;
+	position: relative;
+	width: 120px;
+	height: 68px;
+	cursor: pointer;
+	-webkit-tap-highlight-color: transparent;
+	&:before {
+		content: "";
+		display: block;
+		visibility: visible;
+		width: 120px;
+		height: 68px;
+		background: #9cdaff;
+		box-shadow: 0 0 4px #003399;
+		border-radius: 34px;
+		transition: 0.25s;
+	}
+	&:after {
+		content: "";
+		display: block;
+		visibility: visible;
+		position: absolute;
+		top: 0;
+		width: 52px;
+		height: 52px;
+		margin: 8px;
+		background: linear-gradient(yellow, #ffb300);
+		box-shadow: inset 0 0 2px #d8860b;
+		border-radius: 50%;
+		transition: 0.25s;
+	}
+	&:hover {
+		&:before {
+			box-shadow: 0 0 10px #003399;
+		}
+	}
+	&:checked {
+		&:before {
+			background: #222;
+			box-shadow: 0 0 4px black;
+		}
+		&:after {
+			left: 52px;
+			background: linear-gradient(#777, #3a3a3a);
+			box-shadow: none;
+		}
+		&:hover {
+			&:before {
+				box-shadow: 0 0 10px black;
+			}
+		}
+	}
+}`,
+		"js": ""
+	},
+	"switch-light-dark": {
+		"html": `<label id="switch-light-dark">
+	<input type="checkbox">
+	<div>
+		<svg id="light" viewBox="0 0 16 16">
+			<path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
+		</svg>
+		<svg id="dark" viewBox="0 0 16 16">
+			<path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278zM4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286z"/>
+		</svg>
+	</div>
+</label>`,
+		"css": `#switch-light-dark {
+	width: 120px;
+	height: 68px;
+	position: relative;
+	cursor: pointer;
+	-webkit-tap-highlight-color: transparent;
+	input {
+		position: absolute;
+		visibility: hidden;
+		cursor: pointer;
+		&:before {
+			content: "";
+			display: block;
+			visibility: visible;
+			width: 120px;
+			height: 68px;
+			background: linear-gradient(white, #eee);
+			box-shadow: 0 0 4px black;
+			border-radius: 34px;
+			transition: 0.25s;
+		}
+		&:checked {
+			&:before {
+				background: #222;
+			}
+			+div {
+				left: 52px;
+				background: linear-gradient(#777, #3a3a3a);
+				box-shadow: none;
+				#light {
+					display: none;
+				}
+				#dark {
+					display: block;
+				}	
+			}
+		}	
+	}
+	div {
+		position: absolute;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 52px;
+		height: 52px;
+		margin: 8px;
+		background: linear-gradient(white, #ddd);
+		box-shadow: inset 0 0 2px #999;
+		border-radius: 50%;
+		#light,
+		#dark {
+			width: 36px;
+			height: 36px;
+		}
+		#dark {
+			display: none;
+		}
+	}
+	&:hover {
+		input:before {
+			box-shadow: 0 0 10px black;
+		}
+	}
+}`,
+		"js": ""
+	},
+	"switch-day-night": {
+		"html": `<label id="switch-day-night">
+	<input type="checkbox">
+	<div>
+		<svg id="day" viewBox="0 0 16 16">
+			<path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
+		</svg>
+		<svg id="night" viewBox="0 0 16 16">
+			<path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278zM4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286z"/>
+		</svg>
+	</div>
+</label>`,
+		"css": `#switch-day-night {
+	width: 120px;
+	height: 68px;
+	position: relative;
+	-webkit-tap-highlight-color: transparent;
+	input {
+		position: absolute;
+		visibility: hidden;
+		cursor: pointer;
+		&:before {
+			content: "";
+			display: block;
+			visibility: visible;
+			width: 120px;
+			height: 68px;
+			background: #9cdaff;
+			border-radius: 34px;
+			transition: 0.25s;
+		}
+		&:after {
+			content: "";
+			display: block;
+			visibility: visible;
+			position: absolute;
+			top: 0;
+			z-index: 1;
+			width: 120px;
+			height: 68px;
+			box-shadow: 0 0 4px #003399;
+			border-radius: 34px;
+			transition: 0.25s;
+		}
+		&:hover {
+			&:after {
+				box-shadow: 0 0 10px #003399;
+			}
+		}
+		&:checked {
+			&:before {
+				background: #222;
+			}
+			&:after {
+				box-shadow: 0 0 4px black;	
+			}
+			+div {
+				left: 52px;
+				background: linear-gradient(#777, #3a3a3a);
+				box-shadow: none;
+				#day {
+					display: none;
+				}
+				#night {
+					display: block;
+				}	
+			}
+			&:hover {
+				&:after {
+					box-shadow: 0 0 10px black;
+				}
+			}
+		}	
+	}
+	div {
+		position: absolute;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 52px;
+		height: 52px;
+		margin: 8px;
+		background: linear-gradient(yellow, #ffb300);
+		box-shadow: inset 0 0 2px #d8860b;
+		border-radius: 50%;
+		#day,
+		#night {
+			width: 36px;
+			height: 36px;
+		}
+		#night {
+			display: none;
+		}
+	}
+}`,
+		"js": ""
 	}
 };
