1f5dd5f42d203aee9fb1aef34caa226f4396e54b
ceyur <139768476+ceyur@users.noreply.github.com>
2026-09-02 09:28:14 +0500

    Update code.js

--- code/code.js
+++ code/code.js
@@ -28,12 +28,12 @@ Object.keys(objects).forEach(key => {
 		<button class="bCss">css</button>
 		${js}
 	</div>
-	<input class="html"></input>
-	<input class="css" style="display: none;"></input>
-	<input class="js" style="display: none;"></input>`;
-	div1.querySelector('.html').value = objects[key].html;
-	div1.querySelector('.css').value = objects[key].css;
-	div1.querySelector('.js').value = objects[key].js;
+	<textarea class="html"></textarea>
+	<textarea class="css" style="display: none;"></textarea>
+	<textarea class="js" style="display: none;"></textarea>`;
+	div1.querySelector('.html').textContent = objects[key].html;
+	div1.querySelector('.css').textContent = objects[key].css;
+	div1.querySelector('.js').textContent = objects[key].js;
 	code.appendChild(div1);
 
 	let button = div.querySelector("button");
