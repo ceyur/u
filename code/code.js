6e8f3b151c7c676fb758c62dcedb047ef0c097b6
ceyur <139768476+ceyur@users.noreply.github.com>
2026-09-08 10:36:21 +0500

    Update code.js

--- code/code.js
+++ code/code.js
@@ -54,21 +54,21 @@ Object.keys(objects).forEach(key => {
 		iframe.srcdoc = `${html.value}<style>${styleInIframe}${css.value}</style><script>${js.value}</script>`;
 	});
 	
-	CodeMirror.fromTextArea(html), {
+	CodeMirror.fromTextArea(html, {
 	    mode: "htmlmixed",
 	    theme: "eclipse",
 	    lineNumbers: true,
 	    tabSize: 4
 	});
 	
-	CodeMirror.fromTextArea(css), {
+	CodeMirror.fromTextArea(css, {
 	    mode: "sass",
 	    theme: "eclipse",
 	    lineNumbers: true,
 	    tabSize: 4
 	});
 	
-	CodeMirror.fromTextArea(js), {
+	CodeMirror.fromTextArea(js, {
 	    mode: "javascript",
 	    theme: "eclipse",
 	    lineNumbers: true,
